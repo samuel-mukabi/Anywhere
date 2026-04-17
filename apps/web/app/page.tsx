024f209054a14779563403a65baec58db7e5f149
Samuel Mukabi <mukamau0@gmail.com>
2026-04-17 19:59:08 +0300

    feat: initialize turborepo monorepo

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/app/components/section/Hero";
+
+const Page = () => {
+    return (
+        <main className="flex-1 flex flex-col">
+            <Hero />
+        </main>
+    )
+}
+export default Page
